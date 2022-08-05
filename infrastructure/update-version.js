1ac2fd15a5e15e66ac0c5ac47322ebe83e251571
duysolo <duypt.dev@gmail.com>
2022-08-05 07:31:34 +0700

    update file package.json path

--- infrastructure/update-version.js
+++ infrastructure/update-version.js
@@ -2,11 +2,14 @@
 
 const fs = require('fs')
 
-const raw = fs.readFileSync('./package.json', { encoding: 'utf8' })
+const raw = fs.readFileSync('./../package.json', { encoding: 'utf8' })
 
 const packageContent = JSON.parse(raw)
 
 packageContent.version =
   process.env.NEW_PACKAGE_VERSION || packageContent.version
 
-fs.writeFileSync('./package.json', JSON.stringify(packageContent, undefined, 2))
+fs.writeFileSync(
+  './../package.json',
+  JSON.stringify(packageContent, undefined, 2)
+)
