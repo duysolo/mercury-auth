e4a9090af8cdd14c06ab3af6b1f0317ee694f110
duysolo <duypt.dev@gmail.com>
2022-08-09 11:07:47 +0700

    mark default generic type for AuthRepository as any

--- src/domain/repositories/auth.repository.ts
+++ src/domain/repositories/auth.repository.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs'
 import { IAuthUserEntity } from '..'
 
 @Injectable()
-export abstract class AuthRepository<T> {
+export abstract class AuthRepository<T = any> {
   public abstract getAuthUserByUsername(
     username: string
   ): Observable<IAuthUserEntity<T> | undefined>
